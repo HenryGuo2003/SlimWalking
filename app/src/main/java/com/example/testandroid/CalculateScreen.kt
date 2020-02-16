3011ecbbe6cc74f85eda9c6477563be0cff340e5
Henry's PC <hguo2003@gmail.com>
2020-02-16 14:23:23 -0600

    Add new pages

---
+++ app/src/main/java/com/example/testandroid/CalculateScreen.kt
@@ -0,0 +1,22 @@
+package com.example.testandroid
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_calculate_screen.*
+
+class CalculateScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculate_screen)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+}
